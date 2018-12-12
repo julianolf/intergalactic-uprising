017b02e8aa7efb9ada1d2cd8ad512933353ed8f7
Juliano Fernandes <julianofernandes@gmail.com>
2018-12-12 16:02:07 -0200

    adjust menu to use settings entries, removed a few hard coded constants and switched imports to keep the package namespaces

--- game/menu.py
+++ game/menu.py
@@ -1,51 +1,65 @@
-from pygame import display, event, image
-from pygame.font import Font
-from pygame.time import Clock
+import pygame
+from game import settings
 from kezmenu3 import KezMenu
 
 
 class Menu(object):
-    """Display game options"""
+    """Display game options."""
 
-    def __init__(self, screen, position):
-        self.running = True
-        self.screen = screen
-        self.position = position
+    def __init__(self, game):
+        """Initializes the menu.
+
+        Args:
+            game: The running game instance.
+        """
+        self.game = game
+        self.running = False
+        self.clock = pygame.time.Clock()
 
     def new_game(self):
-        """Start a new game"""
-        pass
+        """Starts a new game."""
+        self.running = False
+        self.game.new()
 
     def exit(self):
-        """Exit game"""
+        """Exit game."""
         self.running = False
 
     def fill_background(self):
-        """Fill screen with a single image"""
-        width = self.screen.get_width()
-        height = self.screen.get_height()
-        background = image.load('game/res/sprite/background/black.png')
+        """Fill screen with a single image."""
+        width = self.game.screen.get_width()
+        height = self.game.screen.get_height()
+        background = pygame.image.load(settings.BLACK_BG_IMG)
 
-        for y in range(0, height, 240):
-            for x in range(0, width, 240):
-                self.screen.blit(background, (x, y))
+        for y in range(0, height, background.get_height()):
+            for x in range(0, width, background.get_width()):
+                self.game.screen.blit(background, (x, y))
 
     def draw(self):
-        """Draw menu"""
+        """Draws menu on screen."""
         self.menu = KezMenu(
             ['NEW GAME', self.new_game],
             ['EXIT', self.exit])
 
-        self.menu.position = self.position
-        self.menu.color = (205, 205, 205, 100)
-        self.menu.focus_color = (255, 255, 255)
-        self.menu.font = Font('game/res/font/kenvector_future.ttf', 32)
+        pos = (
+            (self.game.screen.get_width() / 2 - 100),
+            (self.game.screen.get_height() / 2 - 50)
+        )
+        self.menu.position = pos
+        self.menu.color = settings.MENU_FONT_COLOR
+        self.menu.focus_color = settings.MENU_FONT_FOCUS_COLOR
+        self.menu.font = pygame.font.Font(
+            settings.MENU_FONT,
+            settings.MENU_FONT_SIZE
+        )
         self.menu.enableEffect('raise-col-padding-on-focus', enlarge_time=0.1)
 
-        clock = Clock()
-
+        self.running = True
         while self.running:
-            self.menu.update(event.get(), clock.tick(30) / 1000.)
+            self.menu.update(
+                pygame.event.get(),
+                self.clock.tick(settings.FPS) / 1000.
+            )
             self.fill_background()
-            self.menu.draw(self.screen)
-            display.flip()
+            self.menu.draw(self.game.screen)
+            pygame.display.flip()
--- game/settings.py
+++ game/settings.py
@@ -1,4 +1,26 @@
+import os
+
+# General settings.
 WIDTH = 768
 HEIGHT = 512
 FPS = 30
 BLACK = (0, 0, 0)
+
+# Resources path settings.
+RES_DIR = os.path.join(os.path.dirname(__file__), 'res')
+FNT_DIR = os.path.join(RES_DIR, 'font')
+SND_DIR = os.path.join(RES_DIR, 'sound')
+SPR_DIR = os.path.join(RES_DIR, 'sprite')
+SPR_BG_DIR = os.path.join(SPR_DIR, 'background')
+SPR_EN_DIR = os.path.join(SPR_DIR, 'enemy')
+SPR_PL_DIR = os.path.join(SPR_DIR, 'player')
+SPR_UI_DIR = os.path.join(SPR_DIR, 'ui')
+
+# Image resources.
+BLACK_BG_IMG = os.path.join(SPR_BG_DIR, 'black.png')
+
+# Menu settings.
+MENU_FONT = os.path.join(FNT_DIR, 'kenvector_future.ttf')
+MENU_FONT_SIZE = 32
+MENU_FONT_COLOR = (205, 205, 205)
+MENU_FONT_FOCUS_COLOR = (255, 255, 255)
